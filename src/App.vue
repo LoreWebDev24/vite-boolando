ade2325c501802524620cffdd5ebb600b92a272e
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-11-06 16:18:14 +0100

    started styling the components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
-<script setup>
+<script>
+   import Header from "./components/Header.vue"
+   import MainApp from "./components/MainApp.vue"
+  // VUE
+  export default {
+    data() {
+      return {
+      }
+    },
+    components: {
+      Header,
+      MainApp
+
+    }
+  }
 
 </script>
 
 <template>
   <div>
-   
+    <Header></Header>
+    <MainApp></MainApp>
   </div>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+  @use './styles/master.scss';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,83 @@
+<script>
+      export default {
+    name: 'Header',
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <header class="sticky">
+        <div class="header__container">
+            <div class="row header__row">
+                <div class="col col-2 ">
+                    <ul>
+                        <li><a href="">Woman</a></li>
+                        <li><a href="">Man</a></li>
+                        <li><a href="">kid</a></li>
+                    </ul>
+                </div>
+                <div class="logo-col">
+                    <img class="logo" src="../../public/img/boolean-logo.png" alt="">
+                </div>
+                <div class="col col-2">
+                    <ul>
+                        <li>
+                            <a href="#">Log</a>
+                        </li>
+                        <li>
+                            <a href="#">Liked</a>
+                        </li>
+                        <li>
+                            <a href="#">Cart</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>                        
+  </div>
+</template>
+
+<style scoped>
+
+    header {
+    height: 80px;
+    background-color: #FF6900 ;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding: 0 5px;
+}
+
+.sticky {
+	position: sticky;
+	top: 0;
+	z-index: 999;
+}
+
+.logo {
+    width: 30%;
+	display: block;
+}
+
+.header__container {
+	max-width: 1200px;
+	margin: 0 auto;
+}
+
+.header__row {
+	flex-wrap: nowrap;
+}
+
+.logo-col {
+	display: flex;
+	justify-content: center;
+	flex-basis: calc((100% / 12) * 8);
+}
+
+</style>
