b46b5e327678187c78f67f56f77d8ff01aa70e0d
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-11-09 16:30:33 +0100

    add some comments to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+// VUE IMP 
 import { createApp } from 'vue'
 import App from './App.vue'
+// FONTAWESOME
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCircleXmark } from '@fortawesome/free-regular-svg-icons'
+// ADD + MOUNT 
 library.add(faCircleXmark)
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 
